8028e6d2fcf9f9fd28490a7ba47d5b8a0b3cd280
Adrian Saputra <sputraadrian15@gmail.com>
2025-12-01 22:31:14 +0700

    Add 4_input Java project files
    Initial commit of the 4_input Java project, including build scripts, NetBeans configuration, manifest, source code, and compiled class files. Also removes the Latihan-PBO2 directory.

---
+++ 4_input/src/pkg4_input/Main.java
@@ -0,0 +1,80 @@
+package pkg4_input;
+
+import java.util.Scanner;
+public class Main {
+
+
+    public static void main(String[] args) {
+/*
+        int panjang = 5;
+        int lebar = 10;
+        int luas = panjang * lebar;
+        
+        System.out.println("luas persegi panjang adalah");
+*/          
+   /*
+       int angka;
+       Scanner input = new Scanner (System.in);
+       
+       angka = input.nextInt();
+        System.out.println("hasil dari inputan adalah "+angka);
+     */
+   /*
+     String angka; 
+     String nama;
+     Scanner input = new Scanner (System.in);
+      
+         System.out.println("berapa angka yang akan di input :");
+         angka = input.nextLine();
+         System.out.println("angka yang di inputkan adalah:"+angka);
+         angka = input.nextLine();
+         
+        
+        
+        System.out.println("Nama saya adalalah: ");
+        nama = input.nextLine();
+        System.out.println("hallo, Nama saya adalah:"+nama);
+      */ 
+ 
+   String nama;
+   String alamat;
+   Scanner input = new Scanner (System.in);
+    
+        System.out.println("HALO SAYA ADALAH ADRIAN SAPUTRA SEBAGAI ADMIN PURBAYA: ");
+        System.out.println("SILAHKAN ISI FORM DIBAWAH INI");
+        System.out.println("");
+        System.out.println("=====FROM PENDAFTAR=====");
+        System.out.print("Nama :");
+        nama = input.nextLine();
+        System.out.println("Nama pendaftar: "+nama);
+        System.out.print("Alamat: ");
+        alamat = input.nextLine();
+        System.out.println("Alamat pendaftar: "+alamat);
+        System.out.println("==========");
+        System.out.println("");
+        System.out.println("=====PROFIL PENDAFTARAN=====");
+        System.out.println("Nama :"+nama);
+        System.out.println("Alamat:"+alamat);
+        System.out.println("==============");
+       
+   
+   /*
+        String nama,alammat;
+        Scanner ian= new Scanner(System.in);
+        System.out.println("halo, saya adalah adminpoltek purbaya");
+        System.out.println("silahkan isi form dibawah ini");
+        System.out.println("");
+        
+        
+        //input
+        System.out.println("=========form daftar========");
+        System.out.print("Nama: ");
+        nama = ian.nextLine();
+        System.out.print("Nama pendaftar adalah: ");
+        nama = ian.nextLine();
+        
+        */
+        
+    }
+    
+}
